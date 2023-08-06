90b58425c4934ec44a2b2609678622cc7aec319a
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-08-06 12:18:22 -0400

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put(content);
+  await tx.complete;
+  console.log('Content added to the database:', content);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.complete;
+  console.log('Content retrieved from the database:', content);
+  return content;
+};
 
 initdb();
